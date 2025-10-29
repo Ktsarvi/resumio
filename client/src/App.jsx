d0da972b581212bc54c47f3aa2b74f677381d473
Ktsarvi <alasgarovtimur@gmail.com>
2025-10-30 00:51:15 +0400

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Preview from "./pages/Preview";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Layout from "./pages/Layout";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+
+        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,9 @@
+const ResumeBuilder = () => {
+    return (
+        <div>
+            <h1>ResumeBuilder</h1>
+        </div>
+    )
+}
+
+export default ResumeBuilder
