02393cb0d3435d9fa9c467004be99c1b4888ee94
Ktsarvi <alasgarovtimur@gmail.com>
2025-11-12 22:03:15 +0400

    implement resume creating/updating and remove background from image

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { registerUser, loginUser, getUserById } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  getUserById,
+  getUserResumes,
+} from "../controllers/userController.js";
 import protect from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
@@ -7,5 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
+userRouter.get("/resumes", protect, getUserResumes);
 
 export default userRouter;
